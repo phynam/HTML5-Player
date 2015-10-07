9bcdf68b9efecfe320a64b680a49dc06d1c91061
phynam <pieter@hynam.net>
2015-10-07 10:49:46 -0400

    Initial commit.

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'dist/assets/css'
+sass_dir = 'src/scss'
+relative_assets = true
+line_comments = false
+
+# output_style = :compressed
---
+++ dist/assets/js/player.js
@@ -0,0 +1,51 @@
+soundManager.setup({
+    url: '../assets/swf/soundmanager2.swf',
+    useHTML5Audio: true,
+    html5Test: 'maybe',
+    preferFlash: false,
+    onready: function() {
+        window.mySound = soundManager.createSound({
+            id: 'sound1',
+            url: 'http://www.stephaniequinn.com/Music/Hungarian%20Dance.mp3',
+            whileplaying: function() {
+                $('[data-player-target="progress"]').css('width', ((this.position / this.duration) * 100) + '%');
+            },
+            whileloading: function() {
+                $('[data-player-target="download"]').css('width', ((this.bytesLoaded / this.bytesTotal) * 100) + '%');
+            },
+            onfinish: function() {
+
+            }
+        });
+    },
+    ontimeout: function() {
+        alert("SM2 could not start. Missing SWF? Flash blocked?");
+    }
+});
+
+var Player = function() {
+    this.$el = $('.player');
+    this.init = function() {
+        var context = this;
+        this.$el.on('click', '.player__control', function() {
+            context.play();
+        });
+        this.$el.on('click', '.player__seek', function(e) {
+            var position_x = $(this).offset().left,
+                offset = e.pageX - position_x,
+                bar_width = $(this).width(),
+                position = parseInt((offset / bar_width) * mySound.duration);
+
+            mySound.setPosition(position);
+        });
+    };
+    this.play = function() {
+        this.$el.toggleClass('is-playing');
+        mySound.togglePause();
+    };
+    this.init();
+};
+
+var player = new Player();
+
+// TODO: Add animation on player hide / show
